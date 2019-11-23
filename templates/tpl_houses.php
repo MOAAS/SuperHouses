eba34d91fac494d53c141fe5cc71b13a4f05e4f4
Daniel <38920805+VextoN@users.noreply.github.com>
2019-11-23 19:14:46 +0000

    css casas / php n ta com db ainda

--- templates/tpl_houses.php
+++ templates/tpl_houses.php
@@ -1,10 +1,48 @@
 <?php function draw_houselist() { ?>
   <section id="houses">
     <ul>
-        <li>Casa 1</li>
-        <li>Casa 2</li>
-        <li>Casa 3</li>
-        <li>Casa 4</li>
+    <li>
+        <figure>
+          <img src="https://freshome.com/wp-content/uploads/2018/09/contemporary-exterior.jpg" alt="House">
+          <figcaption><h3>Casa a beira da praia</h3></figcaption>
+        </figure>
+        <i class="fas fa-map-marker-alt"></i>
+        <p>Rua blablabla 34 blabla</p>
+        <p class="price">94</p>
+    </li>
+    <li>
+        <figure>
+          <img src="https://freshome.com/wp-content/uploads/2018/09/contemporary-exterior.jpg" alt="House">
+          <figcaption><h3>Casa a beira da praia muito fixe a pois</h3></figcaption>
+        </figure>
+        <p>Rua blablabla 34 </p>
+        <p class="price">500</p>
+    </li>
+    <li>
+        <figure>
+          <img src="https://freshome.com/wp-content/uploads/2018/09/contemporary-exterior.jpg" alt="House">
+          <figcaption><h3>Casa a beira da praia</h3></figcaption>
+        </figure>
+        <p>Rua blablabla 34 blabla</p>
+        <p class="price">94</p>
+    </li>
+    <li>
+        <figure>
+          <img src="https://freshome.com/wp-content/uploads/2018/09/contemporary-exterior.jpg" alt="House">
+          <figcaption><h3>Casa a beira da praia</h3></figcaption>
+        </figure>
+        <p>Rua blablabla 34 blabla</p>
+        <p class="price">94</p>
+    </li>
+    <li>
+        <figure>
+          <img src="https://freshome.com/wp-content/uploads/2018/09/contemporary-exterior.jpg" alt="House">
+          <figcaption><h3>Casa a beira da praia</h3></figcaption>
+        </figure>
+        <p>Rua blablabla 34 blabla</p>
+        <p class="price">94</p>
+    </li>
+       
     </ul>
   </section>
 <?php } ?>
