0e6883edbe7578105776e4caf2fbbac92d796bf0
Daniel <38920805+VextoN@users.noreply.github.com>
2019-11-23 19:16:40 +0000

    aa

--- templates/tpl_houses.php
+++ templates/tpl_houses.php
@@ -6,7 +6,6 @@
           <img src="https://freshome.com/wp-content/uploads/2018/09/contemporary-exterior.jpg" alt="House">
           <figcaption><h3>Casa a beira da praia</h3></figcaption>
         </figure>
-        <i class="fas fa-map-marker-alt"></i>
         <p>Rua blablabla 34 blabla</p>
         <p class="price">94</p>
     </li>
