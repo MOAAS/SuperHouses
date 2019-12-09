7be2ee00f942eda1d2cdc879140c3145fae0d5a3
Daniel <38920805+VextoN@users.noreply.github.com>
2019-12-09 09:57:55 +0000

    ULTRA FIX

--- js/profile.js
+++ js/profile.js
@@ -268,26 +268,9 @@ const checkPlacesButton = document.getElementById('checkPlacesButton');
 if (checkPlacesButton != null)
     checkPlacesButton.addEventListener('click', () => selectTabItem('Your places'));
 
-    
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
-}
 
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-    }
-}
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
 toggleSwitch.addEventListener('change', switchTheme, false);
 
 
