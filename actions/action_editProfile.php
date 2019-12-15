35ea26258e64dfc53dd124e1e1ce896bc9377e88
Daniel <38920805+VextoN@users.noreply.github.com>
2019-12-15 20:06:30 +0000

    !

--- actions/action_editProfile.php
+++ actions/action_editProfile.php
@@ -9,17 +9,17 @@
         if ($_FILES['imageUpload']['error'] !== UPLOAD_ERR_OK) {
             addErrorMessage("Profile Image update failed!Upload failed with error code " . $_FILES['file']['error']);
             die(header('Location: ../pages/profile.php#Profile'));
-           }     
-           $info = getimagesize($_FILES['imageUpload']['tmp_name']);
-           if ($info === FALSE) {
-            addErrorMessage("Profile Image update failed! Unable to determine image type of uploaded file");
-            die(header('Location: ../pages/profile.php#Profile'));
-           }
-           
-           if (($info[2] !== IMAGETYPE_GIF) && ($info[2] !== IMAGETYPE_JPEG) && ($info[2] !== IMAGETYPE_PNG)) {
-            addErrorMessage("Profile Image update failed!Not a gif/jpeg/png");
-            die(header('Location: ../pages/profile.php#Profile'));
-           }
+        }     
+        $info = getimagesize($_FILES['imageUpload']['tmp_name']);
+        if ($info === FALSE) {
+         addErrorMessage("Profile Image update failed! Unable to determine image type of uploaded file");
+         die(header('Location: ../pages/profile.php#Profile'));
+        }
+
+        if (($info[2] !== IMAGETYPE_GIF) && ($info[2] !== IMAGETYPE_JPEG) && ($info[2] !== IMAGETYPE_PNG)) {
+         addErrorMessage("Profile Image update failed!Not a gif/jpeg/png");
+         die(header('Location: ../pages/profile.php#Profile'));
+        }
         $uploaddir = "../database/profileImages/";
         $user = getUserInfo($_SESSION['username']);
         $id = getUserID($user->username);
