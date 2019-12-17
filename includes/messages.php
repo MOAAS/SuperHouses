93699f5d5c9ec167576ce5c61971373e6af84ee4
MOAAS <miguel.moas@gmail.com>
2019-12-17 09:24:57 +0000

    bug fixes on messages

--- includes/messages.php
+++ includes/messages.php
@@ -18,7 +18,7 @@
 
         public function makeSendTime($sendTime) {
             $datetime = DateTime::createFromFormat('Y-m-d H:i:s', $sendTime);
-            $now = new DateTime('now');
+
             
             
             $day = $datetime->format('j');
@@ -27,9 +27,11 @@
 
             $hours = $datetime->format('H');
             $minutes = $datetime->format('i');
-                        
-            $diff = $now->diff($datetime);
-
+            
+            $date = DateTime::createFromFormat('Y-m-d', $datetime->format('Y-m-d'));
+            $now = new DateTime('now');
+            $diff = $now->diff($date);
+            
             if ($diff->y > 0 || $diff->m > 0 || $diff->d > 1)
                 return $day . " of " . $monthName . ", " . $year;
             if ($diff->d == 1)
